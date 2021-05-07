bf7fcf85ac802d2989af3e7f1030d444f2146ba5
LifeInvader <matthewkausfox@hotmail.com>
2021-05-07 12:28:49 -0600

    added fetch api

---
+++ assets/js/homepage.js
@@ -0,0 +1,7 @@
+var getUserRepos = function () {
+    var response = fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+        console.log("inside", response);
+    });
+};
+console.log("outside");
+getUserRepos();
